a6a54b311a51371c5b49a468c01556fd5e2b1b12
yaser <y.r.k.r.2011@gmail.com>
2023-07-16 21:55:01 +0200

    repetitve element detection correction

--- Draft1001.java
+++ Draft1001.java
@@ -10,21 +10,23 @@ public class Draft1001 {
 	static void finder(int[] mylist, int element) {
 		List<Integer> list = new ArrayList<>();
 		int j = 0;
+
 		for (int i = 0; i < mylist.length; i++) {
 			if (mylist[i] == element) {
-				while (mylist[i] == element) {
+				while (i < mylist.length && mylist[i] == element) {
 					j++;
 					i++;
 				}
 				list.add(j);
 				j = 0;
 			}
+
 		}
 		System.out.println(list);
 	}
 
 	public static void main(String[] args) {
-		int[] mynumbers = { 1, 1, 5, 10, 1, 1, 1, 1, 3, 3, 5, 0, 1, 1, 80 };
+		int[] mynumbers = { 1, 1, 5, 10, 8, 100, 1, 1, 1, 1 };
 		System.out.println(Arrays.toString(mynumbers) + mynumbers.length);
 		finder(mynumbers, 1);
 
